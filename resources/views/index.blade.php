eae6d6ca91fa1b75f3302250f7c17e577ea30d63
ariekas <ariariekas@gmail.com>
2025-04-15 21:09:03 +0700

    update : marge project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,7 +49,7 @@
                 <p class="text-sm font-light">Add tasks, organize them into lists, and track your progress.</p>
             </div>
         </div>
-        <div class="flex flex-col md:flex-row md:justify-between items-center space-x-15 mt-5">
+        <div class="flex flex-col md:flex-row md:justify-between items-center md:space-x-15 mt-5">
             <div>
                 <h1 class="text-lg font-semibold mb-2"># About this To - Do</h1>
                 <p class="text-sm font-light text-justify lg:text-lg">This simple to-do list is designed to help you
@@ -66,13 +66,15 @@
         </div>
 
         <div class="flex flex-col mt-5">
-            <h1 class="text-lg font-normal">Semua Tugas</h1>
+            <div class="flex flex-row justify-between items-center">
+                <h1 class="text-lg font-normal">Semua Tugas</h1>
+                <button class="px-4 py-2 rounded-full border border-gray-700 active:bg-gray-700 active:text-white text-sm md:text-md" data-bs-toggle="modal" data-bs-target="#create-list">Tambah Task</button>
+            </div>
             <div class="w-full overflow-hidden ">
                 <div class="w-full overflow-x-auto">
                     <table class="w-full">
                         <thead>
-                            <tr
-                                class="text-md font-semibold tracking-wide text-left text-gray-600 text-sm">
+                            <tr class="text-md font-semibold tracking-wide text-left text-gray-600 text-sm">
                                 <th class="px-4 py-3">Nama</th>
                                 <th class="px-4 py-3">Deskripsi</th>
                                 <th class="px-4 py-3">Status</th>
@@ -86,7 +88,8 @@
                                 <td class="px-4 py-3 border border-gray-400">
                                     Project Laravel To Do List dengan menggunakan javascript
                                 </td>
-                                <td class="px-4 py-3 text-ms  border border-gray-400">Membuat Project To Do list dengan Tailwind dan laravel</td>
+                                <td class="px-4 py-3 text-ms  border border-gray-400">Membuat Project To Do list dengan
+                                    Tailwind dan laravel</td>
                                 <td class="px-4 py-3 text-xs border border-gray-400">
                                     <span
                                         class="px-3 py-2 font-semibold leading-tight text-green-700 bg-green-100 rounded-lg">
@@ -96,11 +99,13 @@
                                 <td class="px-4 py-3 text-sm border border-gray-400">2025-02-15 20:28:01</td>
                                 <td class="px-4 py-3 text-sm border border-gray-400">
                                     <div class="flex justify-start gap-2 text-md">
-                                      <button class="px-4 py-2 rounded-full border border-[#4291B0] active:bg-[#4291B0] active:text-white">Edit</button>
-                                      <button class="px-4 py-2 rounded-full border border-red-400 active:bg-red-400 active:text-white">Delete</button>
+                                        <button
+                                            class="px-4 py-2 rounded-full border border-[#4291B0] active:bg-[#4291B0] active:text-white">Edit</button>
+                                        <button
+                                            class="px-4 py-2 rounded-full border border-red-400 active:bg-red-400 active:text-white">Delete</button>
                                     </div>
-                                  </td>
-                                  
+                                </td>
+
                             </tr>
                         </tbody>
                     </table>
@@ -111,8 +116,7 @@
                 <div class="w-full overflow-x-auto">
                     <table class="w-full">
                         <thead>
-                            <tr
-                                class="text-md font-semibold tracking-wide text-left text-gray-600 text-sm">
+                            <tr class="text-md font-semibold tracking-wide text-left text-gray-600 text-sm">
                                 <th class="px-4 py-3">Nama</th>
                                 <th class="px-4 py-3">Deskripsi</th>
                                 <th class="px-4 py-3">Status</th>
@@ -126,7 +130,8 @@
                                 <td class="px-4 py-3 border border-gray-400">
                                     Project Laravel To Do List dengan menggunakan javascript
                                 </td>
-                                <td class="px-4 py-3 text-ms  border border-gray-400">Membuat Project To Do list dengan Tailwind dan laravel</td>
+                                <td class="px-4 py-3 text-ms  border border-gray-400">Membuat Project To Do list dengan
+                                    Tailwind dan laravel</td>
                                 <td class="px-4 py-3 text-xs border border-gray-400">
                                     <span
                                         class="px-3 py-2 font-semibold leading-tight text-green-700 bg-green-100 rounded-lg">
@@ -136,11 +141,13 @@
                                 <td class="px-4 py-3 text-sm border border-gray-400">2025-02-15 20:28:01</td>
                                 <td class="px-4 py-3 text-sm border border-gray-400">
                                     <div class="flex justify-start gap-2 text-md">
-                                      <button class="px-4 py-2 rounded-full border border-[#4291B0] active:bg-[#4291B0] active:text-white">Edit</button>
-                                      <button class="px-4 py-2 rounded-full border border-red-400 active:bg-red-400 active:text-white">Delete</button>
+                                        <button
+                                            class="px-4 py-2 rounded-full border border-[#4291B0] active:bg-[#4291B0] active:text-white">Edit</button>
+                                        <button
+                                            class="px-4 py-2 rounded-full border border-red-400 active:bg-red-400 active:text-white">Delete</button>
                                     </div>
-                                  </td>
-                                  
+                                </td>
+
                             </tr>
                         </tbody>
                     </table>
@@ -151,8 +158,7 @@
                 <div class="w-full overflow-x-auto">
                     <table class="w-full">
                         <thead>
-                            <tr
-                                class="text-md font-semibold tracking-wide text-left text-gray-600 text-sm">
+                            <tr class="text-md font-semibold tracking-wide text-left text-gray-600 text-sm">
                                 <th class="px-4 py-3">Nama</th>
                                 <th class="px-4 py-3">Deskripsi</th>
                                 <th class="px-4 py-3">Status</th>
@@ -166,7 +172,8 @@
                                 <td class="px-4 py-3 border border-gray-400 ">
                                     Project Laravel To Do List dengan menggunakan javascript
                                 </td>
-                                <td class="px-4 py-3 text-ms  border border-gray-400">Membuat Project To Do list dengan Tailwind dan laravel</td>
+                                <td class="px-4 py-3 text-ms  border border-gray-400">Membuat Project To Do list dengan
+                                    Tailwind dan laravel</td>
                                 <td class="px-4 py-3 text-xs border border-gray-400">
                                     <span
                                         class="px-3 py-2 font-semibold leading-tight text-white bg-[#64748B] rounded-lg flex justify-start">
@@ -176,11 +183,13 @@
                                 <td class="px-4 py-3 text-sm border border-gray-400">2025-02-15 20:28:01</td>
                                 <td class="px-4 py-3 text-sm border border-gray-400">
                                     <div class="flex justify-start gap-2 text-md">
-                                      <button class="px-4 py-2 rounded-full border border-[#4291B0] active:bg-[#4291B0] active:text-white">Edit</button>
-                                      <button class="px-4 py-2 rounded-full border border-red-400 active:bg-red-400 active:text-white">Delete</button>
+                                        <button
+                                            class="px-4 py-2 rounded-full border border-[#4291B0] active:bg-[#4291B0] active:text-white">Edit</button>
+                                        <button
+                                            class="px-4 py-2 rounded-full border border-red-400 active:bg-red-400 active:text-white">Delete</button>
                                     </div>
-                                  </td>
-                                  
+                                </td>
+
                             </tr>
                         </tbody>
                     </table>
@@ -191,8 +200,7 @@
                 <div class="w-full overflow-x-auto">
                     <table class="w-full">
                         <thead>
-                            <tr
-                                class="text-md font-semibold tracking-wide text-left text-gray-600 text-sm">
+                            <tr class="text-md font-semibold tracking-wide text-left text-gray-600 text-sm">
                                 <th class="px-4 py-3">Nama</th>
                                 <th class="px-4 py-3">Deskripsi</th>
                                 <th class="px-4 py-3">Status</th>
@@ -206,7 +214,8 @@
                                 <td class="px-4 py-3 border border-gray-400">
                                     Project Laravel To Do List dengan menggunakan javascript
                                 </td>
-                                <td class="px-4 py-3 text-ms  border border-gray-400">Membuat Project To Do list dengan Tailwind dan laravel</td>
+                                <td class="px-4 py-3 text-ms  border border-gray-400">Membuat Project To Do list dengan
+                                    Tailwind dan laravel</td>
                                 <td class="px-4 py-3 text-xs border border-gray-400">
                                     <span
                                         class="px-3 py-2 font-semibold leading-tight text-gray-500 bg-[#E2E8F0] rounded-lg flex  justify-start ">
@@ -216,20 +225,44 @@
                                 <td class="px-4 py-3 text-sm border border-gray-400">2025-02-15 20:28:01</td>
                                 <td class="px-4 py-3 text-sm border border-gray-400">
                                     <div class="flex justify-start gap-2 text-md">
-                                      <button class="px-4 py-2 rounded-full border border-[#4291B0] active:bg-[#4291B0] active:text-white">Edit</button>
-                                      <button class="px-4 py-2 rounded-full border border-red-400 active:bg-red-400 active:text-white">Delete</button>
+                                        <button
+                                            class="px-4 py-2 rounded-full border border-[#4291B0] active:bg-[#4291B0] active:text-white">Edit</button>
+                                        <button
+                                            class="px-4 py-2 rounded-full border border-red-400 active:bg-red-400 active:text-white">Delete</button>
                                     </div>
-                                  </td>
-                                  
+                                </td>
+
                             </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-        <div>
-            
+    </div>
+    <div class="w-full p-4 lg:py-5  bg-[#4291B0] mt-5">
+        <div class="flex flex-col md:flex-row md:justify-between gap-5 md:gap-10 mditems-center xl:px-15">
+            <div class="flex flex-col text-white text-md space-y-2 ">
+                <h1 class="font-semibold text-lg">ToList Me</h1>
+                <p class="">This simple to-do list is designed to help you stay focused, organized, and motivated. It’s a space to plan your day, track progress, and celebrate every small win. Whether it’s a big project or a quick reminder, everything starts with writing it down.</p>
+            </div>
+            <div class="flex flex-col md:flex-row space-x-5 md:items-center md:mb-5 gap-5 md:gap-0">
+                <div class="flex flex-col text-start text-white gap-3">
+                    <h1 class="font-semibold text-lg">Company</h1>
+                    <p>About</p>
+                    <p>Blog</p>
+                    <p>Contact</p>
+                </div>
+                <div class="flex flex-col md:flex-row space-x-5 ">
+                    <div class="flex flex-col text-start text-white gap-3">
+                        <h1 class="font-semibold text-lg">Legal</h1>
+                        <p>Privacy</p>
+                        <p>Terms</p>
+                        <p>Security</p>
+                    </div>
+                </div>
+            </div>
         </div>
+        <p class="text-center text-white text-sm md:text-md border-t">Copyright &copy; 2023, All Right Reserved</p>
     </div>
 
 </body>
